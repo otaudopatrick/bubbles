a9821742d6a372e6472790b9724d5e69002da70a
Patrick Luz <patrickluz.jobs@gmail.com>
2020-08-01 00:20:12 -0300

    Corrigindo url das imagens

--- src/components/Bubbles/index.js
+++ src/components/Bubbles/index.js
@@ -5,13 +5,13 @@ import { Container, Bubble } from './styles';
 function Bubbles() {
   return (
     <Container>
-      <Bubble src={process.env.PUBLIC_URL + 'img/bubble.png'} width={'25px'} delay={'2s'} />
-      <Bubble src={process.env.PUBLIC_URL + 'img/bubble.png'} width={'50px'} delay={'1s'} />
-      <Bubble src={process.env.PUBLIC_URL + 'img/bubble.png'} width={'25px'} delay={'3s'}/>
-      <Bubble src={process.env.PUBLIC_URL + 'img/bubble.png'} width={'50px'} delay={'4.5s'}/>
-      <Bubble src={process.env.PUBLIC_URL + 'img/bubble.png'} width={'50px'} delay={'3s'}/>
-      <Bubble src={process.env.PUBLIC_URL + 'img/bubble.png'} width={'20px'} delay={'6s'}/>
-      <Bubble src={process.env.PUBLIC_URL + 'img/bubble.png'} width={'35px'} delay={'7s'}/>
+      <Bubble src={process.env.PUBLIC_URL + '/img/bubble.png'} width={'25px'} delay={'2s'} />
+      <Bubble src={process.env.PUBLIC_URL + '/img/bubble.png'} width={'50px'} delay={'1s'} />
+      <Bubble src={process.env.PUBLIC_URL + '/img/bubble.png'} width={'25px'} delay={'3s'}/>
+      <Bubble src={process.env.PUBLIC_URL + '/img/bubble.png'} width={'50px'} delay={'4.5s'}/>
+      <Bubble src={process.env.PUBLIC_URL + '/img/bubble.png'} width={'50px'} delay={'3s'}/>
+      <Bubble src={process.env.PUBLIC_URL + '/img/bubble.png'} width={'20px'} delay={'6s'}/>
+      <Bubble src={process.env.PUBLIC_URL + '/img/bubble.png'} width={'35px'} delay={'7s'}/>
     </Container>
   );
 }
