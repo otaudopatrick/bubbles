c2a77f29dc8dc47d4effe8a3671b4ebddb0ce550
Patrick Luz <patrickluz.jobs@gmail.com>
2020-07-31 23:05:52 -0300

    Adiciona animção para as bolhas

--- src/components/Bubbles/index.js
+++ src/components/Bubbles/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
-import { Container,Bubble } from './styles';
+import { Container, Bubble } from './styles';
 
 function Bubbles() {
   const style = {
-    width:'50px',
+    width: '50px',
   }
   return (
     <Container>
-      <Bubble src = {process.env.PUBLIC_URL + 'img/bubble.png'} style={style}/>
-      <Bubble src = {process.env.PUBLIC_URL + 'img/bubble.png'} style={style}/>
-      <Bubble src = {process.env.PUBLIC_URL + 'img/bubble.png'} style={style}/>
-      <Bubble src = {process.env.PUBLIC_URL + 'img/bubble.png'} style={style}/>
-      <Bubble src = {process.env.PUBLIC_URL + 'img/bubble.png'} style={style}/>
-      <Bubble src = {process.env.PUBLIC_URL + 'img/bubble.png'} style={style}/>
-      <Bubble src = {process.env.PUBLIC_URL + 'img/bubble.png'} style={style}/>
+      <Bubble src={process.env.PUBLIC_URL + 'img/bubble.png'} style={style} delay={'2s'} />
+      <Bubble src={process.env.PUBLIC_URL + 'img/bubble.png'} style={style} delay={'1s'} />
+      <Bubble src={process.env.PUBLIC_URL + 'img/bubble.png'} style={style} delay={'3s'}/>
+      <Bubble src={process.env.PUBLIC_URL + 'img/bubble.png'} style={style} delay={'4.5s'}/>
+      <Bubble src={process.env.PUBLIC_URL + 'img/bubble.png'} style={style} delay={'3s'}/>
+      <Bubble src={process.env.PUBLIC_URL + 'img/bubble.png'} style={style} delay={'6s'}/>
+      <Bubble src={process.env.PUBLIC_URL + 'img/bubble.png'} style={style} delay={'7s'}/>
     </Container>
   );
 }
--- src/components/Bubbles/styles.js
+++ src/components/Bubbles/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { keyframes } from 'styled-components'
 
 export const Container = styled.div`
   width:100%;
@@ -6,9 +7,28 @@ export const Container = styled.div`
   align-items:center;
   justify-content:space-around;
   position:absolute;
+  bottom:0; 
+`;
+
+const bubbleAnimation = keyframes`
+  0%{
+    transform:translateY(0);
+    opacity:0;
+  }50%{
+    opacity:1;
+  }70%{
+    opacity:1;
+  }100%{
+    transform:translateY(-80vh);
+    opacity:0;
+  }
   
 `;
-
 export const Bubble = styled.img` 
-
+  animation-name: ${bubbleAnimation};
+  animation-duration: 7s;
+  animation-timing-function:linear;
+  animation-delay:${props=>props.delay};
+  animation-iteration-count: infinite;
 `;
+
