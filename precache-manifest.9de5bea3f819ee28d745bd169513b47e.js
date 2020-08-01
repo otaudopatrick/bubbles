376b288b851e9207ee3b7cff107b3dc9217f3807
Patrick Luz <patrickluz.jobs@gmail.com>
2020-08-01 00:05:08 -0300

    Updates

---
+++ precache-manifest.9de5bea3f819ee28d745bd169513b47e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe8d53d439a12333ab4045c186f7543",
+    "url": "/bubbles/index.html"
+  },
+  {
+    "revision": "634c981c2f9e774e8ccb",
+    "url": "/bubbles/static/js/2.faa2855c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bubbles/static/js/2.faa2855c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3112e0fdf9b4a4c8be94",
+    "url": "/bubbles/static/js/main.0d24adc9.chunk.js"
+  },
+  {
+    "revision": "c0b9c15d66984e4471d2",
+    "url": "/bubbles/static/js/runtime-main.c9607de5.js"
+  }
+]);
